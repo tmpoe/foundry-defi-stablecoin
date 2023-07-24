a60c8d91b119633ce35321709f5704190c80d747
tmpITK <torok.mark.patrik@gmail.com>
2023-07-24 17:59:33 +0200

    Add burn and mint

--- src/StableCoin.sol
+++ src/StableCoin.sol
@@ -26,6 +26,7 @@
 pragma solidity ^0.8.20;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
  * @title StableCoin
@@ -37,6 +38,36 @@ import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensio
  * This is a stable coin governed by DSCEngine. This contract is the ERC20 implementation of a stable coin system.
  *
  */
-contract StableCoin is ERC20Burnable {
-    constructor() ERC20("StableCoun", "SC") {}
+contract StableCoin is ERC20Burnable, Ownable {
+    error StableCoin__BurnAmountIsMoreThanBalance();
+    error StableCoin__MustBeMoreThanZero();
+    error StableCoin__ZeroAddress();
+
+    constructor() ERC20("StableCoun", "SC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        if (_amount > balanceOf(msg.sender)) {
+            revert StableCoin__BurnAmountIsMoreThanBalance();
+        }
+
+        if (_amount <= 0) {
+            revert StableCoin__MustBeMoreThanZero();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_amount <= 0) {
+            revert StableCoin__MustBeMoreThanZero();
+        }
+
+        if (_to == address(0)) {
+            revert StableCoin__ZeroAddress();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
